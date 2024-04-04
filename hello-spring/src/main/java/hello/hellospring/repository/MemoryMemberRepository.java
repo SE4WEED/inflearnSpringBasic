1d5e6ec709cfcffb4272922da39968c3a64cecb7
SE4WEED <alsaud0820@naver.com>
2024-04-04 13:12:21 +0900

    section 4

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 import hello.hellospring.domain.Member;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 	private static Map<Long, Member> store = new HashMap<>();
 	private static long sequence = 0l;
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,11 +9,9 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
 public class MemberService {
 	private final MemberRepository memberRepository;
 	
-	@Autowired
 	public MemberService(MemberRepository memberRepository) {
 		super();
 		this.memberRepository = memberRepository;
