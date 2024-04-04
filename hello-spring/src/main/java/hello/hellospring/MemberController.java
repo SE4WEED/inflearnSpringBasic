445b211de297b665919e141fcddebc3d7a47d1d9
SE4WEED <alsaud0820@naver.com>
2024-04-04 12:58:11 +0900

    section4.1 ComponentScan

---
+++ hello-spring/src/main/java/hello/hellospring/MemberController.java
@@ -0,0 +1,18 @@
+package hello.hellospring;
+
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -6,11 +6,14 @@ import java.util.Optional;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
 	private final MemberRepository memberRepository;
 	
-	
+	@Autowired
 	public MemberService(MemberRepository memberRepository) {
 		super();
 		this.memberRepository = memberRepository;
